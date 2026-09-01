6a7eb8ffbdcbd4d54c745a653bbadf2b6e6e804c
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-09-01 22:22:45 +0300

    Implement owner-specific dashboard access control

--- core/views.py
+++ core/views.py
@@ -1,17 +1,21 @@
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import OwnerRequiredMixin
+
+CustomUser = get_user_model()
 
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.user.role == CustomUser.Role.OWNER:
             return redirect("core:dashboard") 
 
         return super().get(request, *args, **kwargs)
 
 
-class DashboardView(LoginRequiredMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, OwnerRequiredMixin, TemplateView):
     template_name = "core/dashboard.html"
