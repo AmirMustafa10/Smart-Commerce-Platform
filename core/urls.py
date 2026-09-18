1f6520d6f5781909f491a2ffd408bcd1f89ed72c
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-09-18 22:34:02 +0300

    Move dashboard functionality from core to a dedicated app and redirect authenticated users from the home page to their dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import HomeView, DashboardView
+from .views import HomeView
 
 app_name = "core"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
-from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 CustomUser = get_user_model()
 
@@ -10,17 +9,12 @@ class HomeView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect("core:dashboard")
+        user = self.request.user
+        if user.is_authenticated:
+            if user.role in (CustomUser.Role.OWNER, CustomUser.Role.MANAGER):
+                return redirect("dashboards:manager_dashboard")
+            else:
+                return redirect("dashboards:shipper_dashboard")
 
         return super().get(request, *args, **kwargs)
 
-
-class DashboardView(LoginRequiredMixin, TemplateView):
-    def get_template_names(self):
-        user = self.request.user
-        if user.role in (CustomUser.Role.OWNER, CustomUser.Role.MANAGER):
-            template_name = "core/manager_dashboard.html"
-        else:
-            template_name = "core/shipper_dashboard.html"
-        return template_name
